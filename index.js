4cd8ebfebb852aac6a95145089f4e1641fb7c85d
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-18 12:43:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, name){
+  let da = drivers.filter(
+    (match) => match.toLowerCase() === name.toLowerCase()
+  )
+  return da
+}
+
+function fuzzyMatch(drivers, name) {
+  const driversArr = drivers.filter(
+      (match) => match.toLowerCase().indexOf(name.toLowerCase()) === 0
+  )
+  return driversArr;
+}
+
+function matchName(driver, name) {
+  const driverArr = driver.filter(
+      (match) => match.name === name
+  )
+  return driverArr;}
